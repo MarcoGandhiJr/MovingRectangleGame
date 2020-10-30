a04fd9e0e042779cadfead1385778ee53f62c000
Xavier Dufour-Maheux <xavierdufourmaheux@gmail.com>
2020-10-30 14:54:12 -0400

    Initial commit

--- src/xavierdufour/Main.java
+++ src/xavierdufour/Main.java
@@ -3,7 +3,5 @@ package xavierdufour;
 public class Main {
 
     public static void main(String[] args) {
-        MovingRectangleGame game = new MovingRectangleGame();
-        game.start();
     }
 }
